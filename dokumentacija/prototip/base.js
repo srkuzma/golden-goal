091303833c7acabe9058033dafa35884fd7d5c31
kovacevicdejan <dejankovacevic10@gmail.com>
2022-03-23 21:23:49 +0100

    Sredjeni linkovi

--- dokumentacija/prototip/base.js
+++ dokumentacija/prototip/base.js
@@ -13,35 +13,82 @@ template.innerHTML = `<div class="jumbotron bg-dark text-warning jumbotron-fluid
                 <a href="https://www.arsenal.com/">
                     <img src="images/arsenal.png" class="logo">
                 </a>
+
                 <a href="https://www.avfc.co.uk/">
                     <img src="images/aston-vila.png" class="logo">
                 </a>
-                <img src="images/brentford.png" class="logo">
-                <img src="images/brighton.png" class="logo">
-                <img src="images/burnley.png" class="logo">
+
+                <a href="https://www.brentfordfc.com/">
+                    <img src="images/brentford.png" class="logo">
+                </a>
+
+                <a href="https://www.brightonandhovealbion.com/">
+                    <img src="images/brighton.png" class="logo">
+                </a>
+
+                <a href="https://www.burnleyfootballclub.com/">
+                    <img src="images/burnley.png" class="logo">
+                </a>
+
                 <a href="https://www.chelseafc.com/en">
                     <img src="images/chelsea.png" class="logo">
                 </a>
-                <img src="images/crystal-palace.png" class="logo">
-                <img src="images/everton.png" class="logo">
-                <img src="images/leeds.png" class="logo">
-                <img src="images/leicester.png" class="logo">
+
+                <a href="https://www.cpfc.co.uk/">
+                    <img src="images/crystal-palace.png" class="logo">
+                </a>
+
+                <a href="https://www.evertonfc.com/home">
+                    <img src="images/everton.png" class="logo">
+                </a>
+
+                <a href="https://www.leedsunited.com/">
+                    <img src="images/leeds.png" class="logo">
+                </a>
+                
+                <a href="https://www.lcfc.com/">
+                    <img src="images/leicester.png" class="logo">
+                </a>
+
                 <a href="https://www.liverpoolfc.com/">
                     <img src="images/liverpool.png" class="logo">
                 </a>
-                <img src="images/manchester-city.png" class="logo">
+                
+                <a href="https://www.mancity.com/">
+                    <img src="images/manchester-city.png" class="logo">
+                </a>
+                
                 <a href="https://www.manutd.com/">
                     <img src="images/manchester-united.png" class="logo">
                 </a>
-                <img src="images/newcastle.png" class="logo">
-                <img src="images/norwitch.png" class="logo">
-                <img src="images/southampton.png" class="logo">
+
+                <a href="https://www.nufc.co.uk/">
+                    <img src="images/newcastle.png" class="logo">
+                </a>
+
+                <a href="https://www.canaries.co.uk/">
+                    <img src="images/norwitch.png" class="logo">
+                </a>
+                
+                <a href="https://www.southamptonfc.com/">
+                    <img src="images/southampton.png" class="logo">
+                </a>
+                
                 <a href="https://www.tottenhamhotspur.com/">
                     <img src="images/tottenhamp.png" class="logo">
                 </a>
-                <img src="images/watford.png" class="logo">
-                <img src="images/westham.png" class="logo">
-                <img src="images/wolves.png" class="logo">
+
+                <a href="https://www.watfordfc.com/">
+                    <img src="images/watford.png" class="logo">
+                </a>
+                
+                <a href="https://www.whufc.com/">
+                    <img src="images/westham.png" class="logo">
+                </a>
+                
+                <a href="https://www.wolves.co.uk/">
+                    <img src="images/wolves.png" class="logo">
+                </a>
             </span>
             <div class="row">
                 <span class="col-9" style="margin-left: 0px;">
